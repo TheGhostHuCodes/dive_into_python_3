4ce647bdaa40bc8bfa97291b76bc5bf627792250
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-08-26 08:10:26 -0700

    Move pluralization rules into their own functions

--- chapter_06_closures_and_generators/pluralize.py
+++ chapter_06_closures_and_generators/pluralize.py
@@ -1,10 +1,43 @@
 import re
 
 
+def match_sxz(noun):
+    return re.search('[sxz]$', noun)
+
+
+def apply_sxz(noun):
+    return re.sub('$', 'es', noun)
+
+
+def match_h(noun):
+    return re.search('[^aeioudgkprt]h$', noun)
+
+
+def apply_h(noun):
+    return re.sub('$', 'es', noun)
+
+
+def match_y(noun):
+    return re.search('[^aeiou]y$', noun)
+
+
+def apply_y(noun):
+    return re.sub('y$', 'ies', noun)
+
+
+def match_default(noun):
+    return True
+
+
+def apply_default(noun):
+    return noun + 's'
+
+
+rules = ((match_sxz, apply_sxz), (match_h, apply_h), (match_y, apply_y),
+         (match_default, apply_default), )
+
+
 def plural(noun):
-    if re.search('[sxz]$', noun) or re.search('[^aeioudgkprt]h$', noun):
-        return re.sub('$', 'es', noun)
-    elif re.search('[^aeiou]y$', noun):
-        return re.sub('y$', 'ies', noun)
-    else:
-        return noun + 's'
+    for matches_rule, apply_rule in rules:
+        if matches_rule(noun):
+            return apply_rule(noun)
