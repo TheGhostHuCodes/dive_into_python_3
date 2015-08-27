bc7321689d01b7cf2ef7e42b5a1570bf67c086d1
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-08-27 09:44:21 -0700

    Implement plural rules as an iterator class

---
+++ chapter_07_classes_and_iterators/lazyrules.py
@@ -0,0 +1,49 @@
+import re
+
+
+def build_match_and_apply_functions(pattern, search, replace):
+    def match_rule(word):
+        return re.search(pattern, word)
+
+    def apply_rule(word):
+        return re.sub(search, replace, word)
+
+    return (match_rule, apply_rule)
+
+
+class LazyRules:
+    def __init__(self, rules_filename):
+        self.pattern_file = open(rules_filename, encoding='utf-8')
+        self.cache = []
+
+    def __iter__(self):
+        self.cache_index = 0
+        return self
+
+    def __next__(self):
+        self.cache_index += 1
+        if len(self.cache) >= self.cache_index:
+            return self.cache[self.cache_index - 1]
+
+        if self.pattern_file.closed:
+            raise StopIteration
+
+        line = self.pattern_file.readline()
+        if not line:
+            self.pattern_file.close()
+            raise StopIteration
+
+        pattern, search, replace = line.split(None, 3)
+        funcs = build_match_and_apply_functions(pattern, search, replace)
+        self.cache.append(funcs)
+        return funcs
+
+
+rules = LazyRules('plural-rules.txt')
+
+
+def plural(noun):
+    for matches_rule, apply_rule in rules:
+        if matches_rule(noun):
+            return apply_rule(noun)
+    raise ValueError('No matching rule for {0}'.format(noun))
